f20598af8607705f3b10d7557acf4df9e416ba28
ph3n1x <sayan.mondal@mastiska.com>
2018-09-19 23:04:45 +0530

    Time function updated

--- time_encrypto.java
+++ time_encrypto.java
@@ -14,8 +14,9 @@ public class time_encrypto {
     public static void main(String[] args) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
         LocalDateTime now = LocalDateTime.now();  
-        System.out.println(dtf.format(now));  
-        StringUtil new1 = new StringUtils(dtf.format(now));  
-        System.out.println(new1);
+        System.out.println(dtf.format(now)); 
+        // Added to encrypt the time  
+        // StringUtil new1 = new StringUtils(dtf.format(now));  
+        // System.out.println(new1);
     }   
 }
