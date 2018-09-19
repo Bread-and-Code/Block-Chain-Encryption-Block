51c7e1c320786a4df1bb98433ce2de9baf9b2058
ph3n1x <sayan.mondal@mastiska.com>
2018-09-19 23:02:04 +0530

    Time function added

---
+++ time_encrypto.java
@@ -0,0 +1,21 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+// import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.time.LocalDateTime;
+
+
+
+/**
+ * time_encrypto
+ */
+public class time_encrypto {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        StringUtil new1 = new StringUtils(dtf.format(now));
+        System.out.println(new1);
+    }
+}
