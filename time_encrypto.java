6f621067f2de323e9c71afedae5e0993f18898e9
ph3n1x <sayan.mondal@mastiska.com>
2018-09-20 23:23:20 +0530

    Time  function updated

--- time_encrypto.java
+++ time_encrypto.java
@@ -1,5 +1,10 @@
+import java.text.DateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+
+// import com.sun.xml.internal.ws.spi.db.TypeInfo;
+
+// import com.sun.glass.ui.Pixels.Format;
 
 // import com.sun.xml.internal.ws.util.StringUtils;
 
@@ -11,10 +16,31 @@ import java.time.LocalDateTime;
  * time_encrypto
  */
 public class time_encrypto {
-    public static void main(String[] args) {
+    static void gettime(){
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(dtf.format(now));
+    }
+    static void getdate(){
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        // System.out.println(TypeInfo(date));
+
+    }
+    static void gettimeonly(){
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+    }
+    static void gethouronly(){
+        LocalDate time = LocalDate.now();
+        System.out.println(time.getDayOfWeek());
+        System.out.println(time.getMonth());
+    }
+    public static void main(String[] args) {
+        getdate();
+        gettime();
+        gettimeonly();
+        gethouronly();
         // Added to encrypt the time
         // StringUtil new1 = new StringUtils(dtf.format(now));
         // System.out.println(new1);
